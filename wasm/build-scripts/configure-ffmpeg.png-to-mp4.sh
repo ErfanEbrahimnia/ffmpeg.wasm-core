87dd11abe5d2a7e99bf59eee070e67f437e589cf
Erfan Ebrahimnia <erfan.ebrahimnia@gmail.com>
2023-06-23 01:04:32 +0200

    removed mp4 handling

--- wasm/build-scripts/configure-ffmpeg.png-to-mp4.sh
+++ wasm/build-scripts/configure-ffmpeg.png-to-mp4.sh
@@ -7,7 +7,7 @@ FLAGS=(
   "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
   --disable-all
   --enable-gpl            # required by x264
-  --enable-libx264        # enable x264
+  #--enable-libx264        # enable x264
   --enable-zlib           # enable zlib
   --enable-avcodec
   --enable-avformat
@@ -15,11 +15,11 @@ FLAGS=(
   --enable-swresample
   --enable-swscale
   --enable-decoder=png
-  --enable-encoder=png,libx264,gif
-  --enable-parser=h264,png,gif
+  --enable-encoder=png,gif
+  --enable-parser=png,gif
   --enable-protocol=file
   --enable-demuxer=image2
-  --enable-muxer=mp4,gif
+  --enable-muxer=gif
   --enable-filter=scale,format,fps,palettegen,paletteuse,null
 
 )
